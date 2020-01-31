e6a3ca54f6371b4ad5d19abe3e1393bd45f6a4c3
pericabartulovic <60116043+pericabartulovic@users.noreply.github.com>
2020-01-31 11:14:39 +0100

    Add files via upload

---
+++ dbh.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+$servername = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$baza = "loginsystempro";
+
+try {
+    $konekcija = new PDO("mysql:host=$servername;dbname=$baza", $dBUsername, $dBPassword);
+} catch (PDOException $ex) {
+    echo ("Nismo se uspjeli spojiti na bazu.");
+    die();
+}
+
+
+
+/* $konekcija = mysqli_connect($servername, $dBUsername, $dBPassword, $baza);
+
+if (!$konekcija) {
+    die("Nismo se uspjeli povezati: ".mysqli_connect_error());
+} */
