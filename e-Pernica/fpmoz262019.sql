6f0a43848889b8572e503ed9baad2555e082b1b3
perica <pericab35@gmail.com>
2020-05-01 03:11:09 +0200

    update baze 01/05/2020

--- e-Pernica/fpmoz262019.sql
+++ e-Pernica/fpmoz262019.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2020 at 02:42 AM
+-- Generation Time: May 01, 2020 at 03:04 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
