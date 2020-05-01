61f92370c93c25d37ea97170ceb4ccde346db1a4
perica <pericab35@gmail.com>
2020-05-02 00:35:43 +0200

    js&design update 02/05/2020

--- e-Pernica/js/jquery.js
+++ e-Pernica/js/jquery.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
    
   if(localStorage.getItem("prviPut")==null){
-    alert("Ova aplikacija je samo informativnog karaktera i služi kao pripomoć u školi");
+    alert("Ova aplikacija je samo informativnog karaktera i služi kao pripomoć u školovanju");
     localStorage.setItem("prviPut","done");
  }
  
